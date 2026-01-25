62f06d2829c0e4c467cb411a04be3a4d11c1ef62
yuuhikaze <yuuhikaze@protonmail.com>
2026-01-25 11:34:28 -0500

    chore: final environment sync and cleanup

--- flake.nix
+++ flake.nix
@@ -4,17 +4,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    tern-src = {
+      url = "github:yuuhikaze/tern";
+    };
     pandoc-resources = {
       url = "github:yuuhikaze/pandoc-resources";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, pandoc-resources, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, tern-src, pandoc-resources, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
         };
 
         pythonEnv = pkgs.python3.withPackages (ps: with ps; [
@@ -28,20 +33,7 @@
           httpx
         ]);
 
-        tern = pkgs.stdenv.mkDerivation {
-          pname = "tern";
-          version = "1.7.0";
-          src = pkgs.fetchurl {
-            url = "https://github.com/yuuhikaze/tern/releases/download/v1.7.0/tern";
-            sha256 = "8513e762741ca4538f1d42bf9bd43d52a3019b393b36e6a19cd1790a773fbd7f";
-          };
-          phases = [ "installPhase" ];
-          installPhase = ''
-            mkdir -p $out/bin
-            cp $src $out/bin/tern
-            chmod +x $out/bin/tern
-          '';
-        };
+        tern-core = tern-src.packages.${system}.tern-core;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -51,30 +43,10 @@
             pkgs.docker-compose
             pkgs.git
             pkgs.pandoc
-            tern
-          ] ++ (with pkgs; [
-            fontconfig
-            wayland
-            libxkbcommon
-            libGL
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-          ]);
+            tern-core
+          ];
 
           shellHook = ''
-            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (with pkgs; [
-              wayland
-              libxkbcommon
-              xorg.libX11
-              xorg.libXcursor
-              xorg.libXrandr
-              xorg.libXi
-              fontconfig
-              libGL
-            ])}
-
             # Setup Tern Converter
             mkdir -p ~/.local/share/tern/converters
             ln -sf ${pandoc-resources}/tern-converters/pandoc.lua ~/.local/share/tern/converters/pandoc.lua
@@ -92,7 +64,7 @@
             echo "======= PSET1 Development Environment ======="
             echo "Python: $(python --version)"
             echo "Docker: $(docker --version)"
-            echo "Tern:   $(tern --version)"
+            echo "Tern:   $(tern-core --version)"
             echo "Pandoc: $(pandoc --version | head -n 1)"
             echo "Resources synced ✓"
             echo "============================================="
